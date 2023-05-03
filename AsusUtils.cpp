88e8147d00140e11a6d8587b394a60284b77bd7a
Devin Carraway <devincarr@gmail.com>
2023-05-02 19:50:18 -0700

    Add second screen toggle

--- AsusUtils.cpp
+++ AsusUtils.cpp
@@ -1,23 +1,79 @@
 #include <cstdint>
-#include <format>
 #include <functional>
 #include <string>
 #include <unistd.h>
 #include <unordered_map>
 
-static void SetSecondScreenBrightness(int32_t argc, char* argv[])
+const static std::string c_SecondScreenBrightnessPath =
+    "/sys/devices/platform/asus-nb-wmi/screenpad_brightness";
+
+const static std::string c_SecondScreenEnabledPath =
+    "/sys/devices/platform/asus-nb-wmi/screenpad_enable";
+
+static void SetSecondScreenBrightness(const std::string& brightness)
+{
+    const std::string command =
+        "echo " + brightness + " | tee " + c_SecondScreenBrightnessPath;
+
+    system(command.c_str());
+}
+
+static void SetSecondScreenBrightnessCLI(int32_t argc, char* argv[])
 {
     if (argc > 2)
     {
-        const std::string command =
-            "echo " + std::string(argv[2]) +
-            " | tee /sys/devices/platform/asus-nb-wmi/screenpad_brightness";
-        system(command.c_str());
+        SetSecondScreenBrightness(argv[2]);
+    }
+}
+
+static void SetSecondScreenEnabled(const std::string& screenEnabled)
+{
+    const std::string command =
+        "echo " + screenEnabled + " | tee " + c_SecondScreenEnabledPath;
+
+    system(command.c_str());
+}
+
+static void SetSecondScreenEnabledCLI(int32_t argc, char* argv[])
+{
+    if (argc > 2)
+    {
+        SetSecondScreenEnabled(argv[2]);
+    }
+}
+
+static void CycleSecondScreenModesCLI(int32_t argc, char* argv[])
+{
+    const std::string secondScreenEnabledCommand = "cat " + c_SecondScreenEnabledPath;
+    const int32_t secondScreenIsEnabled = system(secondScreenEnabledCommand.c_str());
+
+    if (secondScreenIsEnabled == 0)
+    {
+        SetSecondScreenEnabled("1");
+        SetSecondScreenBrightness("100");
+    }
+    else
+    {
+        const std::string secondScreenBrightnessCommand =
+            "cat " + c_SecondScreenBrightnessPath;
+        const int32_t screenBrightness = system(secondScreenBrightnessCommand.c_str());
+
+        if (screenBrightness > 0)
+        {
+            SetSecondScreenBrightness("0");
+        }
+        else
+        {
+            SetSecondScreenEnabled("0");
+        }
     }
 }
 
 static std::unordered_map<std::string, std::function<void(int32_t, char*[])>> s_Commands {
-    {"SetSecondScreenBrightness", &SetSecondScreenBrightness}};
+    {"SetSecondScreenBrightness", &SetSecondScreenBrightnessCLI},
+    {"SetSecondScreenEnabled", &SetSecondScreenEnabledCLI},
+    {"CycleSecondScreenModes", &CycleSecondScreenModesCLI},
+};
 
 int32_t main(int32_t argc, char* argv[])
 {
