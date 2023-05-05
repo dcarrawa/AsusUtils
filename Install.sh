00974bd61fe2c9c27aa7198722da803b29de639a
Devin Carraway <devincarr@gmail.com>
2023-05-05 16:19:38 -0700

    Rename udev rule for proper order.

--- Install.sh
+++ Install.sh
@@ -2,5 +2,5 @@
 
 g++ AsusUtils.cpp -g -o AsusUtils
 
-sudo cp asusutils-backlight.rules /etc/udev/rules.d/asusutils-backlight.rules
+sudo cp 45-asusutils-backlight.rules /etc/udev/rules.d/45-asusutils-backlight.rules
 sudo usermod -aG video $USER
